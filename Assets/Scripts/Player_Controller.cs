48d404e2587608a3f75f983715e09c205ccc6fe9
Nosequep <poupapitas86@gmail.com>
2020-04-10 19:35:28 -0800

    Avance base
    Se agrega la base del juego. Incluimos la escena del primer escenario de introducción, el jugador con sus movimientos y animaciones, y ademas la animación de la cámara al iniciar.

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float moveSpeed;
+    private float dirX;
+    private bool facingRight = true;
+    private Vector3 localScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        localScale = transform.localScale;
+        moveSpeed = 1f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dirX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        if (Input.GetButtonDown("Jump") && rb.velocity.y == 0)
+            rb.AddForce(Vector2.up * 75f);
+
+        if (Mathf.Abs(dirX) > 0 && rb.velocity.y == 0)
+            anim.SetBool("isRunning", true);
+        else
+            anim.SetBool("isRunning", false);
+
+        if(rb.velocity.y == 0)
+        {
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isFalling", false);
+        }
+
+        if (rb.velocity.y > 0)
+            anim.SetBool("isJumping",true);
+
+        if(rb.velocity.y < 0)
+        {
+            anim.SetBool("isJumping", false);
+            anim.SetBool("isFalling", true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(dirX, rb.velocity.y);
+    }
+
+    private void LateUpdate()
+    {
+        if (dirX > 0)
+            facingRight = true;
+        else if (dirX < 0)
+            facingRight = false;
+
+        if (((facingRight) && (localScale.x < 0)) || ((!facingRight) && (localScale.x > 0)))
+            localScale.x *= -1;
+
+        transform.localScale = localScale;
+    }
+}
